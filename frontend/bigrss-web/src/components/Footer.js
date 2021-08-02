6a0f4ea0e3384cc23d493a128c1b59e8281a91aa
Mozzo1000 <andreas@simplymozzo.se>
2021-08-02 18:20:09 +0200

    Add noreferrer to link

--- frontend/bigrss-web/src/components/Footer.js
+++ frontend/bigrss-web/src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="footer">
-            <p>Powered by <a href="https://github.com/Mozzo1000/bigrss" target="_blank">BigRSS</a></p>
+            <p>Powered by <a href="https://github.com/Mozzo1000/bigrss" target="_blank" rel="noreferrer">BigRSS</a></p>
         </div>
     )
 }
